f4c6b0d128c77647cd5e762308f050151c127ab5
Lucas Jacinto <lucasjdseng@gmail.com>
2022-11-02 14:03:45 -0300

    conslusao do projeto

---
+++ script.js
@@ -0,0 +1,42 @@
+//funcao evento listener para ouvir as teclas
+document.body.addEventListener('keyup', event => {
+  playSound(event.code.toLowerCase())
+})
+
+document.querySelector('.composer button').addEventListener('click', () => {
+  let song = document.querySelector('#input').value
+
+  if (song !== '') {
+    let songArrey = song.split('')
+    playComposition(songArrey)
+  }
+})
+
+function playSound(sound) {
+  let audioElement = document.querySelector(`#s_${sound}`)
+  let keyElement = document.querySelector(`div[data-key="${sound}"]`)
+
+  if (audioElement) {
+    audioElement.currentTime = 0
+    audioElement.play()
+  }
+  //adicionando e removendo css
+  if (keyElement) {
+    keyElement.classList.add('active')
+
+    setTimeout(() => {
+      keyElement.classList.remove('active')
+    }, 300)
+  }
+}
+
+function playComposition(songArrey) {
+  let wait = 0
+  for (let songItem of songArrey) {
+    setTimeout(() => {
+      playSound(`key${songItem}`)
+    }, wait)
+
+    wait += 250
+  }
+}
